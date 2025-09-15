155b5de244b14a24d8d6949401dffca40d9be5a1
Jackson <contact@jacksonc.dev>
2025-09-15 18:58:07 +1200

    add meta-data back

--- ubuntu/gen-iso.sh
+++ ubuntu/gen-iso.sh
@@ -1,5 +1,6 @@
 rm -f seed.iso
 mkdir -p seed
 cp user-data.yaml seed/user-data
+cp meta-data.yaml seed/meta-data
 hdiutil makehybrid -o seed.iso seed -iso -joliet -default-volume-name cidata
 rm -rf seed
